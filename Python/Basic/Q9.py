ce65e582877c3782af17fa33f24c6fa845acd547
Blairex <jerryblair0927@gmail.com>
2025-08-16 01:13:52 +0530

    Done basic Q7,8,9

--- Python/Basic/Q9.py
+++ Python/Basic/Q9.py
@@ -1,7 +1,6 @@
 # Implement a program that checks if a number is prime using a while loop for repeated checks and breaks out of the loop when a prime number is found.
 
 num = int(input("Enter a number: "))
-
 if num <= 1:
     print(num, "is not a prime number.")
 else:
