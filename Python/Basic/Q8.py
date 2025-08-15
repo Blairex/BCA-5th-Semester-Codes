2699cf0153e676ab37692712a47af9d40b2f1d2f
Blairex <jerryblair0927@gmail.com>
2025-08-16 01:13:52 +0530

    Done basic Q7,8,6

---
+++ Python/Basic/Q8.py
@@ -0,0 +1,9 @@
+# Design a program that accepts user input as a string and converts it into different datatypes (int, float) using type conversion functions.
+
+user_input = input("Enter a number: ")
+
+int_value = int(user_input)
+print("Integer value:", int_value)
+
+float_value = float(user_input)
+print("Float value:", float_value)
---
+++ Python/Basic/Q9.py
@@ -0,0 +1,15 @@
+# Implement a program that checks if a number is prime using a while loop for repeated checks and breaks out of the loop when a prime number is found.
+
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print(num, "is not a prime number.")
+else:
+    i = 2
+    while i <= num // 2:
+        if num % i == 0:
+            print(num, "is not a prime number.")
+            break
+        i += 1
+    else:
+        print(num, "is a prime number.")
