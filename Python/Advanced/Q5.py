55ed70ffa877342cad8a80a0bc48523152460808
Blairex <jerryblair0927@gmail.com>
2025-08-21 23:09:06 +0530

    Add Q5: dictionary keys and values printer

---
+++ Python/Advanced/Q5.py
@@ -0,0 +1,9 @@
+# 5. Write a function to print all keys and values from a dictionary in separate lists.
+def print_dict(d):
+    keys = list(d.keys())
+    values = list(d.values())
+    print("Keys:", keys)
+    print("Values:", values)
+
+data = {"a": 1, "b": 2, "c": 3}
+print_dict(data)
