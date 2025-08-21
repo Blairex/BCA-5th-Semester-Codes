e85fad2f817e93bb5b4d951d3ce70aa748aac035
Blairex <jerryblair0927@gmail.com>
2025-08-21 23:09:06 +0530

    Add Q6: weekday checker

---
+++ Python/Advanced/Q6.py
@@ -0,0 +1,8 @@
+# 6. Write a program that takes user input for a day and checks if it's a weekday using a tuple.
+weekdays = ("monday", "tuesday", "wednesday", "thursday", "friday")
+
+day = input("Enter a day: ").lower()
+if day in weekdays:
+    print(day, "is a weekday")
+else:
+    print(day, "is not a weekday")
