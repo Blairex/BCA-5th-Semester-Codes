3721843651de87b03951a9d47c34b41c8e44db49
Blairex <jerryblair0927@gmail.com>
2025-08-21 23:09:05 +0530

    Add Q3: list length calculator basic version

---
+++ Python/Advanced/Q3.py
@@ -0,0 +1,9 @@
+# 3. Create a program that takes multiple lists as input and calculates the length of each list using a function.
+def list_length(lst):
+    return len(lst)
+
+list1 = input("Enter elements of list1 (comma separated): ").split(",")
+list2 = input("Enter elements of list2 (comma separated): ").split(",")
+
+print("Length of list1:", list_length(list1))
+print("Length of list2:", list_length(list2))
