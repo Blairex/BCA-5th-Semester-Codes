8fc255e6aaac6da8f220daacb78a32c224ff3b56
Blairex <jerryblair0927@gmail.com>
2025-08-21 23:09:06 +0530

    Update Q3: added minor note

--- Python/Advanced/Q3.py
+++ Python/Advanced/Q3.py
@@ -7,3 +7,4 @@ list2 = input("Enter elements of list2 (comma separated): ").split(",")
 
 print("Length of list1:", list_length(list1))
 print("Length of list2:", list_length(list2))
+# Minor update to Q3
